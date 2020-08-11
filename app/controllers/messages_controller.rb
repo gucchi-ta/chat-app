0137eb08bc400feb3591c69328232e8b189ef2d6
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-08-11 21:54:58 +0900

    チャットルームの削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,6 +18,8 @@ class MessagesController < ApplicationController
     end
   end
 
+ 
+
 
 
   private
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,10 @@
 class Room < ApplicationRecord
   has_many :room_users
-  has_many :users, through: :room_users
-  has_many :messages
+  has_many :users, through: :room_users, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  # 「dependent: :destroyオプション」とは、親モデルが削除された時、それに紐付ている子モデルも一緒に削除されるというオプション。
+  #ここで注意なのが、あくまでも「チャットルームに紐づいているユーザー情報を削除」なので、「メンバーの削除＝中間テーブルから該当するレコード削除」ということになる。
+  #つまり一度登録したユーザー情報はusersテーブルに残ったまま。
 
   validates :name, presence: true 
 end
