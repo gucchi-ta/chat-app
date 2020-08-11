1880c6168969ac83f38c2b88a6ec63fe960c6350
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-08-11 23:19:24 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,9 @@ class Message < ApplicationRecord
   has_one_attached :image
   #「has_one_attached」で、各レコードと画像ファイルを1対1の関係で紐づけることができる。
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?    # テキストが存在していなくても画像の投稿を可能にする。
+  end
 end
